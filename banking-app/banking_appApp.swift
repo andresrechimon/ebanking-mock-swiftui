810ab94b796d440da0125e654bbf4ce2ad61cb42
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-05-22 10:56:15 -0300

    Initial Commit

---
+++ banking-app/banking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  banking_appApp.swift
+//  banking-app
+//
+//  Created by Andrés Rechimon on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct banking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
